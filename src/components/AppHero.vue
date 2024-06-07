332679253b89a725bcef41f13ff84b08f37ba419
giordanofabrizii <giordanofabrizi@gmail.com>
2024-06-07 13:42:54 +0200

    Added Jumbo

---
+++ src/components/AppHero.vue
@@ -0,0 +1,19 @@
+<script>
+
+</script>
+
+<template>
+    <section class="jumbo">
+        <img src="../assets/img/jumbo.png" alt="Jumbo image">
+    </section>
+</template>
+
+<style scoped>
+    img{
+        width: 100%;
+    }
+    section {
+        width: 120rem;
+        margin: 0 auto;
+    }
+</style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,15 @@
+<script setup>
+
+</script>
+
+<template>
+  <section>
+    <h1>La mia prima app con Vite!</h1>
+  </section>
+</template>
+
+<style scoped>
+  h1 {
+    text-align: center;
+  }
+</style>
